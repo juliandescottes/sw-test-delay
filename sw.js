ce64625680f8ec90ad2b556224c35055e2dd8531
Julian Descottes <julian.descottes@gmail.com>
2016-02-29 16:14:40 +0100

    tmp

--- sw.js
+++ sw.js
@@ -8,15 +8,15 @@ this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(wait(5000)).then(function(cache) {
       return cache.addAll([
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
+        '/sw-test-delay/',
+        '/sw-test-delay/index.html',
+        '/sw-test-delay/style.css',
+        '/sw-test-delay/app.js',
+        '/sw-test-delay/image-list.js',
+        '/sw-test-delay/star-wars-logo.jpg',
+        '/sw-test-delay/gallery/bountyHunters.jpg',
+        '/sw-test-delay/gallery/myLittleVader.jpg',
+        '/sw-test-delay/gallery/snowTroopers.jpg'
       ]);
     })
   );
@@ -33,6 +33,6 @@ this.addEventListener('fetch', function(event) {
     });
     return response.clone();
   }).catch(function() {
-    return caches.match('/sw-test/gallery/myLittleVader.jpg');
+    return caches.match('/sw-test-delay/gallery/myLittleVader.jpg');
   }));
 });
